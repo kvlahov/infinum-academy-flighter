3c442306cdb73ed5d7e6b5fa839defe8529b113d
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-07-12 13:01:23 +0200

    Add test to check case insensitivity

--- app/components/open_weather_map/resolver.rb
+++ app/components/open_weather_map/resolver.rb
@@ -4,7 +4,8 @@ module OpenWeatherMap
   module Resolver
     def self.city_id(name)
       data = JSON.parse(File.read(File.expand_path('city_ids.json', __dir__)))
-      data.select { |city| city['name'] == name }.map { |city| city['id'] }.first
+      data.select { |city| city['name'] == name.downcase.capitalize }
+          .map { |city| city['id'] }.first
     end
   end
 end
--- spec/components/open_weather_map/resolver_spec.rb
+++ spec/components/open_weather_map/resolver_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe OpenWeatherMap::Resolver do
   it 'checks if returns nill for invalid name' do
     expect(described_class.city_id('blahaa')).to eq(nil)
   end
+
+  it 'checks if city name is case insensitive' do
+    expect(described_class.city_id('sIBenIk')).to eq(3_190_941)
+  end
 end

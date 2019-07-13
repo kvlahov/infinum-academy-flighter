0957c928f001d3773e27cf3546971046f24ad64e
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-07-13 17:15:24 +0200

    Implement OpenWeatherModule.city method

---
+++ app/components/open_weather_map.rb
@@ -0,0 +1,14 @@
+require 'faraday'
+require 'json'
+
+module OpenWeatherMap
+  def self.city(name)
+    city_id = OpenWeatherMap::Resolver.city_id(name)
+    return nil if city_id.nil?
+
+    response =
+      Faraday.get('http://api.openweathermap.org/data/2.5/weather?id=' + city_id.to_s + '&appid=' +
+      Rails.application.credentials[:open_weather_map_api_key])
+    OpenWeatherMap::City.parse(JSON.parse(response.body))
+  end
+end

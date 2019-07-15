b17f5f23df8b73b792ddaf58fbc45cce41d8a7d0
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-07-15 12:06:43 +0200

    Create migrations

---
+++ db/migrate/20190715095328_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.integer :no_of_seats
+      t.integer :seat_price
+      t.belongs_to :user, index:true, foreign_key:true
+      t.belongs_to :flight, index:true, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end

a92047960fec4a89e34ef19bcba0f9a32d02873d
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-07-15 13:43:51 +0200

    Setup models, install factory, shuoulda matchers

---
+++ app/models/booking.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id          :bigint           not null, primary key
+#  no_of_seats :integer
+#  seat_price  :integer
+#  user_id     :bigint
+#  flight_id   :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Booking < ApplicationRecord
+  validates :no_of_seats, :seat_price, presence: true,
+                                       numericality: { greater_than: 0 }
+  validates :flight_not_in_past?
+  belongs_to :user
+  belongs_to :flight
+
+  def flight_not_in_past?
+    return if flight.flys_at > DateTime.current
+
+    errors['flights.flys_at'] << 'flights can\'t be in the past'
+  end
+end
---
+++ app/models/flight.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: flights
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  no_of_seats :integer
+#  base_price  :integer
+#  flys_at     :datetime
+#  lands_at    :datetime
+#  company_id  :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Flight < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false, scope: :company_id }
+  validates :flys_at, :land_at, presence: true
+  validates :flys_at_before_lands_at?
+  validates :base_price, presence: true,
+                         numericality: { greater_than: 0 }
+
+  belongs_to :company
+
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
+
+  def flys_at_before_lands_at?
+    return if flys_at < lands_at
+
+    errors.add(:flys_at, 'must be before ' + :lands_at.to_s)
+  end
+end

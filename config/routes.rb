233195b7653ac48ada98a43c33ed3c28cca8c08a
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-07-20 17:20:22 +0200

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'api/bookings#index'
   namespace :api do
     resources :bookings, only: [:index, :create, :show, :update, :destroy]
     resources :users, only: [:index, :create, :show, :update, :destroy]

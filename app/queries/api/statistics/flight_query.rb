6f97144ea05bbd02442f36c37f53181ccf0cefbd
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2019-08-02 12:15:59 +0200

    Fix flights statistics formatting

--- app/queries/api/statistics/flight_query.rb
+++ app/queries/api/statistics/flight_query.rb
@@ -8,10 +8,9 @@ module Api
       end
 
       def with_stats
-        relation.joins(:bookings)
-                .select('sum(bookings.seat_price * bookings.no_of_seats) as revenue')
-                .select('sum(bookings.no_of_seats) as no_of_booked_seats')
-                .select('flights.no_of_seats')
+        relation.left_joins(:bookings)
+                .select('coalesce(sum(bookings.seat_price * bookings.no_of_seats), 0) as revenue')
+                .select('coalesce(sum(bookings.no_of_seats), 0) as no_of_booked_seats')
                 .select('flights.*')
                 .group('flights.id')
       end
